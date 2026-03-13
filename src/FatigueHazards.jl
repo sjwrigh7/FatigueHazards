3de55259a0a8db959c9626120e48cd157e67df1e
sjwrigh7 <sjwrigh7@gmail.com>
2026-03-13 14:46:03 -0400

    add additional files to main module

--- src/FatigueHazards.jl
+++ src/FatigueHazards.jl
@@ -1,5 +1,17 @@
 module FatigueHazards
 
+using Distributions
+using ProgressMeter
+using Optim, ADTypes
 # Write your package code here.
+
+include("prelude.jl")
+include("fatigue_models.jl")
+include("data_synthesis.jl")
+include("splines.jl")
+include("hazard_model.jl")
+include("posterior_samplers.jl")
+include("mcmc.jl")
+include("entropy.jl")
 
 end
