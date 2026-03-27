79ad6639b397d60b8a360071389061b463ef7cc9
sjwrigh7 <sjwrigh7@gmail.com>
2026-03-27 08:35:54 -0400

    fix bug with log vs non-log in bilinear material model

--- src/fatigue_models.jl
+++ src/fatigue_models.jl
@@ -13,19 +13,20 @@ function BilinearMaterial(s_max::Real,s_min::Real,n_max::Real,n_min::Real)
     slope = delta_n / delta_s
 
     return BilinearMaterial(
-        log(s_max),
-        log(s_min),
-        log(n_max),
-        log(n_min),
+        log(10,s_max),
+        log(10,s_min),
+        log(10,n_max),
+        log(10,n_min),
         Float64(slope)
     )
 end
 
 function bilinear_sn(material::BilinearMaterial,s::Float64)
-    if s < material.s_max
-        log_ds = log(10,s) - material.s_min
+    log_s = log(10,s)
+    if log_s < material.s_max
+        log_ds = log_s - material.s_min
 
-        log_n = material.n_max + slope * log_ds
+        log_n = material.n_max + material.slope * log_ds
         return 10 ^ log_n
     else
         return 10 ^ material.n_min
