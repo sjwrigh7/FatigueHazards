ba5ac18187643f3bea6bdff6490b6035b8b3ed9b
sjwrigh7 <sjwrigh7@gmail.com>
2026-04-07 08:26:33 -0400

    add new implementation of beta posterior sampler that modifies preallocated arrays instead of allocating new arrays on each call to improve performance

--- src/posterior_samplers.jl
+++ src/posterior_samplers.jl
@@ -107,3 +107,72 @@ function metropolis_beta(beta,M,I_diff,stresses,fail_indic,delta_i,J,gamma,step)
 
     return return_beta,accept,return_risk
 end
+
+# more efficient metropolis function
+function metropolis_beta!(main_risk,off_risk,beta,M,I_diff,stresses,fail_indic,in_risk_idx,J,gamma,step)
+    current_beta = beta
+    current_transformed = log(beta)
+    proposed_transformed = rand(Normal(current_transformed,step))
+    proposed_beta = exp(proposed_transformed)
+
+    #println("Current beta = $current_beta")
+    #println("Proposed beta = $proposed_beta")
+    #proposed_beta = current_beta + rand(Normal(0.0,step))
+    #current_risk = [sum_risk(j,stresses,current_beta,delta_i) for j in 2:(J-1)]
+    #proposed_risk = [sum_risk(j,stresses,proposed_beta,delta_i) for j in 2:(J-1)]
+    @inbounds for j in eachindex(main_risk)
+        main_risk[j] = sum_risk(j+1,stresses,current_beta,in_risk_idx)
+        off_risk[j] = sum_risk(j+1,stresses,proposed_beta,in_risk_idx)
+    end
+    #println("Current risk values")
+    #println(main_risk')
+    #println("Proposed risk values")
+    #println(off_risk')
+    #current_lik = log_lik_splines(stresses,delta_i,T,current_beta,gamma,M,I)
+    #proposed_lik = log_lik_splines(stresses,delta_i,T,proposed_beta,gamma,M,I)
+
+    current_lik = log_lik(stresses,gamma,M,I_diff,J,main_risk,fail_indic)
+    proposed_lik = log_lik(stresses,gamma,M,I_diff,J,off_risk,fail_indic)
+    log_jump_current = logpdf(
+        Normal(
+            current_transformed,
+            step
+        ),
+        proposed_transformed
+    ) - log(proposed_beta)
+    
+    log_jump_proposed = logpdf(
+        Normal(
+            proposed_transformed,
+            step
+        ),
+        current_transformed
+    ) - log(current_beta)
+    
+    log_lik_ratio = proposed_lik - current_lik
+    log_jump_ratio = log_jump_proposed - log_jump_current
+    
+    acceptance_ratio = min(
+        exp(
+            log_lik_ratio + log_jump_ratio
+        ),
+        1.0
+    )
+
+    u = rand(Uniform(0.0,1.0))
+    accept = u <= acceptance_ratio
+
+    if accept
+        return_beta = proposed_beta
+        #return_risk = proposed_risk
+        main_risk[:] .= off_risk
+    else
+        return_beta = current_beta
+        #return_risk = current_risk
+    end
+    #println("Acceptance is $accept")
+    #println("New risk values are")
+    #println(main_risk')
+
+    return return_beta,accept
+end
